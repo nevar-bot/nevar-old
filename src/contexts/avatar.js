59d3725e1617df1d3340847877c1f0ff1734672a
Jonas <p429344@gmail.com>
2023-02-22 00:34:05 +0100

    :sparkles: add: avatar context menu

--- src/contexts/avatar.js
+++ src/contexts/avatar.js
@@ -10,8 +10,24 @@ class Avatar extends BaseContext {
         })
     }
 
+    static interaction;
     async dispatch(interaction){
-        // todo
+        this.interaction = interaction;
+        await this.showAvatar(interaction.targetUser);
+    }
+    async showAvatar(user){
+        const x64 = user.displayAvatarURL({ extension: "png", size: 64 });
+        const x128 = user.displayAvatarURL({ extension: "png", size: 128 });
+        const x256 = user.displayAvatarURL({ extension: "png", size: 256 });
+        const x512 = user.displayAvatarURL({ extension: "png", size: 512 });
+        const x1024 = user.displayAvatarURL({ extension: "png", size: 1024 });
+        const x2048 = user.displayAvatarURL({ extension: "png", size: 2048 });
+
+        const avatarEmbed = this.client.generateEmbed("Links: [x64]({0}) • [x128]({1}) • [x256]({2}) • [x512]({3}) • [x1024]({4}) • [x2048]({5})", null, "normal", x64, x128, x256, x512, x1024, x2048);
+        avatarEmbed.setTitle("Avatar von " + user.tag);
+        avatarEmbed.setImage(x256);
+
+        return this.interaction.followUp({ embeds: [avatarEmbed] });
     }
 }
 
