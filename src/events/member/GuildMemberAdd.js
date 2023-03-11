cd94fd40f16c2d277295f0f865bd50b03a9f8d87
Jonas <p429344@gmail.com>
2023-03-11 21:09:10 +0100

    :bug: bug fix: show correct created difference in join log

--- src/events/member/GuildMemberAdd.js
+++ src/events/member/GuildMemberAdd.js
@@ -17,13 +17,13 @@ module.exports = class {
         });
 
         // Log to member log
-        const createdAt = moment(member.createdTimestamp).format("DD.MM.YYYY HH:mm");
-        const createdDiff = this.client.utils.getRelativeTime(member.createdTimestamp);
+        const createdAt = moment(member.user.createdTimestamp).format("DD.MM.YYYY HH:mm");
+        const createdDiff = this.client.utils.getRelativeTime(member.user.createdTimestamp);
 
         const logText =
             " **" + member.user.tag + " hat den Server betreten**\n\n" +
-            this.client.emotes.calendar + " Account erstellt am: **" + createdAt + "**";
-           // this.client.emotes.reminder + " Account erstellt vor: **" + createdDiff + "**";
+            this.client.emotes.calendar + " Account erstellt am: **" + createdAt + "**\n" +
+            this.client.emotes.reminder + " Account erstellt vor: **" + createdDiff + "**";
 
         await guild.logAction(logText, "guild", this.client.emotes.events.member.unban, "success", member.user.displayAvatarURL({ dynamic: true }));
 
