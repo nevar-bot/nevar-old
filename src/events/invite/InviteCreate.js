4ef8dc37214cecb18dd46e5d8b42e246de1ecbce
Jonas <p429344@gmail.com>
2023-03-11 21:20:06 +0100

    :rocket: improvement: show unlimited instead of 0

--- src/events/invite/InviteCreate.js
+++ src/events/invite/InviteCreate.js
@@ -12,7 +12,7 @@ module.exports = class {
             " ** Einladung " + invite.code + " wurde erstellt**\n\n" +
             this.client.emotes.link + " Link: **" + invite.url + "**\n" +
             this.client.emotes.user + " Erstellt von: **" + invite.inviter.tag + "**\n" +
-            this.client.emotes.reload + " Maximale Verwendungen: ** " + invite.maxUses + "**\n" +
+            this.client.emotes.reload + " Maximale Verwendungen: ** " + (invite.maxUses === 0 ? "Unbegrenzt" : invite.maxUses)  + "**\n" +
             (invite.expiresTimestamp ? this.client.emotes.reminder + " Ablaufdatum: **" + moment(invite.expiresTimestamp).format("DD.MM.YYYY HH:mm") + "**" : "");
 
         return guild.logAction(logText, "guild", this.client.emotes.events.guild.update, "success");
