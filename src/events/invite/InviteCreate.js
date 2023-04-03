8a02f1fc2027e61f53c8b67fbd14dfa77c63bb34
Jonas <p429344@gmail.com>
2023-04-03 20:30:42 +0200

    :ambulance: critical hotfix: cannot read properties of null (reading 'tag')

--- src/events/invite/InviteCreate.js
+++ src/events/invite/InviteCreate.js
@@ -11,7 +11,7 @@ module.exports = class {
         const logText =
             " ** Einladung " + invite.code + " wurde erstellt**\n\n" +
             this.client.emotes.link + " Link: **" + invite.url + "**\n" +
-            this.client.emotes.user + " Erstellt von: **" + invite.inviter.tag + "**\n" +
+            this.client.emotes.user + " Erstellt von: **" + (invite.inviter?.tag ? invite.inviter.tag : "Unbekannt#0000") + "**\n" +
             this.client.emotes.reload + " Maximale Verwendungen: ** " + (invite.maxUses === 0 ? "Unbegrenzt" : invite.maxUses)  + "**\n" +
             (invite.expiresTimestamp ? this.client.emotes.reminder + " Ablaufdatum: **" + moment(invite.expiresTimestamp).format("DD.MM.YYYY HH:mm") + "**" : "");
 
