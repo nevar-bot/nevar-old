f2692db4c40fc6dd148ad51ab363a8482432bf1e
Jonas <p429344@gmail.com>
2023-05-21 16:57:55 +0200

    :rocket: api improvement

--- src/api/controllers/client/stats_controller.js
+++ src/api/controllers/client/stats_controller.js
@@ -1,4 +1,6 @@
 const { ChannelType } = require("discord.js");
+const fs = require("fs");
+const moment = require("moment");
 
 async function get(req, res){
     const { app } = req;
@@ -12,8 +14,10 @@ async function get(req, res){
             shard_count: client.shard ? client.shard.count : 1,
             user_count: client.guilds.cache.reduce((sum, guild) => sum + (guild.available ? guild.memberCount : 0), 0),
             channel_count: client.channels.cache.size,
+            command_count: client.commands.size,
+            vote_count: JSON.parse(fs.readFileSync('./assets/votes.json'))[moment().format("MMMM").toLowerCase()] || 0,
             support_url: client.config.support["INVITE"],
-            invite_url: client.createInvite(),
+            invite_url: client.createInvite()
         }
     }
     res.setHeader('Content-Type', 'application/json');
