d9d2ccf81e6091ec8304c8722538720a016cb8da
Florian Ohldag <mrminecrafttv19@gmail.com>
2023-06-22 11:25:06 +0200

    :rocket: «» Added: pull command (pull code from github and restart bot)

---
+++ src/commands/staff/pull.js
@@ -0,0 +1,48 @@
+const BaseCommand = require('@structures/BaseCommand');
+
+class Pull extends BaseCommand {
+    constructor(client){
+        super(client, {
+            name: "pull",
+            description: "Pull den neuen Code von GitHub und starte den Bot neu.",
+
+            cooldown: 3000,
+            staffOnly: true,
+            dirname: __dirname,
+
+            slashCommand: {
+                addCommand: false,
+            }
+        });
+    }
+
+    static message;
+    async dispatch(message, args, data){
+        this.message = message;
+        await this.pull();
+    }
+
+    async pull(){
+        console.log("Pulling from GitHub...");
+        const pullEmbed = this.client.createEmbed("Es wird von GitHub gepullt..", "warning", "warning", );
+        this.message.reply({ embeds: [pullEmbed] });
+        const { exec } = require('child_process');
+        exec('git pull', (err, stdout, stderr) => {
+            if (err) {
+                console.error(err);
+                let errorEmbed = this.client.createEmbed(`Es ist ein Fehler aufgetreten!\`\`\`${err}\`\`\``, "error", "error", );
+                this.message.reply({ embeds: [errorEmbed] });
+                return;
+            }
+            console.log(stdout);
+            console.log(stderr);
+            let successEmbed = this.client.createEmbed("Erfolgreich von GitHub gepullt! Der Bot wird jetzt neugestartet.", "success", "success", );
+            this.message.reply({ embeds: [successEmbed] });
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        });
+    }
+}
+
+module.exports = Pull;
